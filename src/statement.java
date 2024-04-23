b3e5f7a1e44186918dcb68871b8c3b3f2838c55e
Kevin Kimanzi <kevinkimanzi4@gmail.com>
2024-04-23 18:36:30 +0200

    added my statement file

---
+++ src/statement.java
@@ -0,0 +1,22 @@
+public class statement {
+    public static void main(String[] args) {
+        //if statement
+        int age = 75;
+
+        if (age>=18 && age <75){
+            System.out.println("You are an adult");
+
+        }
+        else if (age>=13){
+            System.out.println("You are teenager");
+        }
+        else if (age>=75){
+            System.out.println("Ok Boomer");
+        }
+       
+        else {
+            System.out.println("You are not an adult");
+        }
+
+		}
+}
