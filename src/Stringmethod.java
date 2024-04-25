6bd67414dba6b81fdac09a10db76e53246bb275b
Kevin Kimanzi <kevinkimanzi4@gmail.com>
2024-04-25 12:57:21 +0200

    added my stringmethod file

---
+++ src/Stringmethod.java
@@ -0,0 +1,12 @@
+public class Stringmethod {
+    public static void main(String[] args){
+        String name = "Kevin";
+        boolean results = name.equalsIgnoreCase("Kevin");
+        int length = name.length();
+        boolean emptyness = name.isEmpty();
+        System.out.println(results);
+        System.out.println(length);
+        System.out.println(emptyness);
+
+    }
+}
