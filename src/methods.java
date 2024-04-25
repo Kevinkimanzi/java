d40bfb548efc042df714d9fbaabeb1d9dd0b7992
Kevin Kimanzi <kevinkimanzi4@gmail.com>
2024-04-25 14:34:52 +0200

    added method file

---
+++ src/methods.java
@@ -0,0 +1,39 @@
+//besicaly methods are chunk of code thet you run in one line
+//assume this is the long version
+
+public class methods {
+    public static void main(String[] args){
+        welcome();
+        multiply(4,5);
+        divide(4,5);
+
+    }
+
+    /*public static void main(String[] args){
+        int a = 10;
+        int b = 10;
+        System.out.println(a * b);
+
+        int c = 13;
+        int d = 16;
+        System.out.println(c * d);
+       
+
+    }
+    */
+    // the method version
+    
+    public static void welcome(){
+        System.out.println("Welcome to my Simple Calculator");
+        }
+        public static void multiply(int a, int b){
+            System.out.println(a * b);
+        }
+    
+        public static void divide(int a, int b){
+            System.out.println(a / b);
+           
+    
+        }
+
+}
