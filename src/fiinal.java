a8fc91eb061d4cbedd19c22d510c5099e99c214d
Kevin Kimanzi <kevinkimanzi4@gmail.com>
2024-04-26 18:19:16 +0200

    added my final file

---
+++ src/fiinal.java
@@ -0,0 +1,10 @@
+// WHAT IS FINAL CANNOT BE CHANGE OR UPDATED
+
+public class fiinal{
+    public static void main(){
+       final double PI = 3.14;
+       System.out.println(PI);
+
+    }
+    
+}
