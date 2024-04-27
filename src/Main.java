c600fe2e95dae5c9c29d024027821769050fc46f
Kevin Kimanzi <kevinkimanzi4@gmail.com>
2024-04-27 17:52:19 +0200

    Added main java file

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("I love pizza");
-		System.out.println("first code in java");
-		
-		// this is a comment
-		int x =71232123;
-		double y = 2123.00;
-
-		boolean b = true;
-		float k = 71232123.00f;
-		char symbol = 'j';
-		String name = "KEVIN KIMANZI";
-		System.out.println(x);
-		System.out.println("Hello my name is "+name+ " and my number is"+""+" "+ x);
-			
-
-		
-		}
+        DiceRolle diceroller = new DiceRolle();
+    }
 }
