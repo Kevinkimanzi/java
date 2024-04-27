a1c88c78fc07e0897de5dcc1b2c012b4e600f66e
Kevin Kimanzi <kevinkimanzi4@gmail.com>
2024-04-27 17:51:26 +0200

    modifieD Human class

--- src/Human.java
+++ src/Human.java
@@ -1,10 +1,21 @@
 public class Human {
     String name;
+    String fname;
+    String lname;
     int age;
     double weight;
 
     Human(String name, int age, double weight){
         this.name=name;
+        this.age=age;
+        this.weight=weight;
+    }
+    // overloading constructor
+    //multiple constructor within a class with same name but different parameters
+    
+    Human(String fname,String lname, int age, double weight){
+        this.fname=fname;
+        this.lname=lname;
         this.age=age;
         this.weight=weight;
     }
