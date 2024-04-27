3ef8093104e6cf85570f97ed31a5e18db4cb9e6b
Kevin Kimanzi <kevinkimanzi4@gmail.com>
2024-04-27 17:52:35 +0200

    Added constructor java file

--- src/constructor1.java
+++ src/constructor1.java
@@ -3,7 +3,13 @@ public class constructor1 {
         Human human1 =new Human("Kevin", 70, 50);
         Human human2 =new Human("Kimanzi", 40, 50);
 
-        System.out.println(human1.name);  //not for attribute we have to print
+        System.out.println(human2.name);
+
+
+        //testing overloading constructor
+        Human human3 =new Human("Kimanzi","Kimanzi", 40, 50);
+
+        System.out.println(human3.fname);  //not for attribute we have to print
 
         human1.eat();  //for method we just call
         System.out.println(); // to allow us have space
